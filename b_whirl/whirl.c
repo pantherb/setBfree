pantherb/setBfree

commit 87d0ceb594190ed1c3a7cffc5962d4b3ae2129cc

whirl: horn mic-angle/width parameter

b_whirl/whirl.c
@@ -74,6 +74,7 @@ void initValues (struct b_whirl *w) {
 	/* The current angle of rotating elements */
 	w->hornAngleGRD = 0; /* 0..1 */
 	w->drumAngleGRD = 0;
+	w->micAngle = 0;
 
 	/* angular speed  grad / sample */
 	w->hornIncr     = 0; /* horn's current angular speed */
@@ -542,7 +543,6 @@ void computeOffsets (struct b_whirl *w) {
 		if (maxdr < w->drFwdDispl[i]) maxdr = w->drFwdDispl[i];
 	}
 
-	// TODO expose mic placement
 	w->hornPhase[0] = 0;
 	w->hornPhase[1] = WHIRL_DISPLC_SIZE >> 1;
 
@@ -1153,6 +1153,8 @@ void whirlProc2 (struct b_whirl *w,
   double hornAngleGRD = w->hornAngleGRD;
   double drumAngleGRD = w->drumAngleGRD;
   unsigned int outpos = w->outpos;
+  const double fwAng = w->micAngle * .25;
+  const double bwAng = 1. + w->micAngle * -.25;
 
   const float leakage = w->leakage;
   const float hornLevel = w->hornLevel;
@@ -1182,7 +1184,7 @@ void whirlProc2 (struct b_whirl *w,
   float * const z = w->z;
 
   const struct _bw * const bfw = w->bfw;
-  const struct _bw * const bbw = w->bfw;
+  const struct _bw * const bbw = w->bbw;
 
 
 #ifdef DEBUG_SPEED
@@ -1210,15 +1212,13 @@ void whirlProc2 (struct b_whirl *w,
     DI = (DI + AGMASK) & AGMASK; \
     DX[DI] = XS;}
 
-#define HN_MOTION(P,BUF,DSP,BW,DX,DI) {                                    \
-    const float h1 = hornAngleGRD * WHIRL_DISPLC_SIZE + hornPhase[(P)];    \
+#define HN_MOTION(P,BUF,DSP,BW,DX,DI,ANG) {                                \
+    const float h1 = (ANG) * WHIRL_DISPLC_SIZE + hornPhase[(P)];           \
     const float hd = fmod(h1, 1.0);                                        \
     const unsigned int hl = (unsigned int)floor(h1) & WHIRL_DISPLC_MASK;   \
     const unsigned int hh = (hl + 1) & WHIRL_DISPLC_MASK;                  \
     const float intp = DSP[hl] * (1.f - hd) + hd * DSP[hh];                \
-    const unsigned int k = (unsigned int)                                  \
-        (round(hornAngleGRD * WHIRL_DISPLC_MASK) + hornPhase[(P)])         \
-        & WHIRL_DISPLC_MASK;                                               \
+    const unsigned int k = (unsigned int) floor(hd);                       \
     const float t = hornSpacing[(P)] + intp + (float) outpos;              \
     const float r = floorf (t);                                            \
     float xa;                                                              \
@@ -1298,24 +1298,24 @@ void whirlProc2 (struct b_whirl *w,
 
     /* --- STATIC HORN FILTER --- */
     /* HORN PRIMARY */
-    HN_MOTION(0, HLbuf, hnFwdDispl, bfw, adx0, w->adi0);
-    HN_MOTION(1, HRbuf, hnBwdDispl, bbw, adx0, w->adi0);
+    HN_MOTION(0, HLbuf, hnFwdDispl, bfw, adx0, w->adi0, hornAngleGRD + fwAng);
+    HN_MOTION(1, HRbuf, hnBwdDispl, bbw, adx0, w->adi0, hornAngleGRD + bwAng);
     ADDHIST(adx0, w->adi0, x);
 
     /* HORN FIRST REFLECTION FILTER */
     FILTER_C(0.4, 0.4, 0);
 
     /* HORN FIRST REFLECTION */
-    HN_MOTION(2, HLbuf, hnBwdDispl, bbw, adx1, w->adi1);
-    HN_MOTION(3, HRbuf, hnFwdDispl, bfw, adx1, w->adi1);
+    HN_MOTION(2, HLbuf, hnBwdDispl, bbw, adx1, w->adi1, hornAngleGRD + fwAng);
+    HN_MOTION(3, HRbuf, hnFwdDispl, bfw, adx1, w->adi1, hornAngleGRD + bwAng);
     ADDHIST(adx1, w->adi1, x);
 
     /* HORN SECOND REFLECTION FILTER */
     FILTER_C(0.4, 0.4, 1);
 
     /* HORN SECOND REFLECTION */
-    HN_MOTION(4, HLbuf, hnFwdDispl, bfw, adx2, w->adi2);
-    HN_MOTION(5, HRbuf, hnBwdDispl, bbw, adx2, w->adi2);
+    HN_MOTION(4, HLbuf, hnFwdDispl, bfw, adx2, w->adi2, hornAngleGRD + fwAng);
+    HN_MOTION(5, HRbuf, hnBwdDispl, bbw, adx2, w->adi2, hornAngleGRD + bwAng);
     ADDHIST(adx2, w->adi2, x);
 
     /* 1A) do doppler shift for drum (actually orig signal -- FM

b_whirl/b_whirl-configurable.ttl.in
@@ -599,6 +599,18 @@
 			rdf:value -1.0;
 		];
 		pp:displayPriority 800 ;
+	],
+	[
+		a lv2:InputPort ,
+			lv2:ControlPort ;
+		lv2:index 41 ;
+		lv2:symbol "micangle" ;
+		lv2:name "MicAngle";
+		lv2:default 180.0 ;
+		lv2:minimum 0.0 ;
+		lv2:maximum 180.0 ;
+		lv2:portProperty pp:causesArtifacts;
+		pp:displayPriority 50 ;
 	];
 	rdfs:comment "A rotating louspeaker emulator designed to imitate the sound and properties of the sound modification device that brought world-wide fame to the name of Don Leslie" ;
 	.

b_whirl/gui/b_whirl.c
@@ -80,7 +80,8 @@ typedef enum {
 	B3W_HORNANG,
 	B3W_DRUMANG,
 	B3W_GUINOTIFY,
-	B3W_LINKSPEED,
+	B3W_LINKSPEED, // 40
+	B3W_MICANGLE,
 } PortIndex;
 
 

b_whirl/lv2.c
@@ -85,7 +85,8 @@ typedef enum {
 	B3W_DRUMANG,
 
 	B3W_GUINOTIFY,
-	B3W_LINKSPEED,
+	B3W_LINKSPEED, // 40
+	B3W_MICANGLE,
 } PortIndex;
 
 typedef struct {
@@ -110,7 +111,7 @@ typedef struct {
 	float *drum_width, *horn_leak;
 
 	float *horn_radius, *drum_radius;
-	float *horn_xoff, *horn_zoff, *mic_dist;
+	float *horn_xoff, *horn_zoff, *mic_dist, *mic_angle;
 
 	float *p_resend_trigger; // GUI retrigger
 	float *p_link_speed; // GUI setting
@@ -130,7 +131,7 @@ typedef struct {
 	float o_drum_width, o_horn_leak;
 
 	float o_horn_radius, o_drum_radius;
-	float o_horn_xoff, o_horn_zoff, o_mic_dist;
+	float o_horn_xoff, o_horn_zoff, o_mic_dist, o_mic_angle;
 
 	int spd_horn, spd_drum, last_spd;
 
@@ -159,15 +160,15 @@ instantiate (const LV2_Descriptor*     descriptor,
              const char*               bundle_path,
              const LV2_Feature* const* features)
 {
-  B3W* b3w = (B3W*)calloc (1, sizeof (B3W));
-  if (!b3w) { return NULL ;}
+	B3W* b3w = (B3W*)calloc (1, sizeof (B3W));
+	if (!b3w) { return NULL ;}
 
-  if (!(b3w->whirl = allocWhirl ())) {
-    free (b3w);
-    return NULL;
-  }
+	if (!(b3w->whirl = allocWhirl ())) {
+		free (b3w);
+		return NULL;
+	}
 
-  initWhirl (b3w->whirl, NULL, rate);
+	initWhirl (b3w->whirl, NULL, rate);
 
 	// reference filters
 	b3w->flt[0].W[0] = b3w->whirl->hafw;
@@ -191,34 +192,34 @@ instantiate (const LV2_Descriptor*     descriptor,
 	b3w->flt[2]._g = b3w->whirl->lpG;
 	b3w->flt[2]._q = b3w->whirl->lpQ;
 
-  b3w->o_horn_radius = b3w->whirl->hornRadiusCm;
-  b3w->o_drum_radius = b3w->whirl->drumRadiusCm;
-  b3w->o_mic_dist = b3w->whirl->micDistCm;
-  b3w->o_horn_xoff = b3w->whirl->hornXOffsetCm;
-  b3w->o_horn_zoff = b3w->whirl->hornZOffsetCm;
+	b3w->o_horn_radius = b3w->whirl->hornRadiusCm;
+	b3w->o_drum_radius = b3w->whirl->drumRadiusCm;
+	b3w->o_mic_dist = b3w->whirl->micDistCm;
+	b3w->o_horn_xoff = b3w->whirl->hornXOffsetCm;
+	b3w->o_horn_zoff = b3w->whirl->hornZOffsetCm;
 	b3w->o_horn_leak = b3w->whirl->leakLevel;
 
-  b3w->fade_dir = false;
-  b3w->fade = 0;
+	b3w->fade_dir = false;
+	b3w->fade = 0;
 
-  b3w->rate = rate;
-  b3w->nyq  = rate * 0.4998;
-  b3w->lpf1  = 2000.0 / rate;
-  b3w->lpf2  = 880.0 / rate;
+	b3w->rate = rate;
+	b3w->nyq  = rate * 0.4998;
+	b3w->lpf1  = 2000.0 / rate;
+	b3w->lpf2  = 880.0 / rate;
 
-  b3w->resend_data_to_ui = 0;
-  b3w->resend_trigger = 0;
+	b3w->resend_data_to_ui = 0;
+	b3w->resend_trigger = 0;
 
 	// fade in levels
-  b3w->o_horn_level = 0.0;
-  b3w->o_drum_level = 0.0;
-  b3w->o_drum_width = 0.0;
+	b3w->o_horn_level = 0.0;
+	b3w->o_drum_level = 0.0;
+	b3w->o_drum_width = 0.0;
 
-  b3w->x_drum_width = 0.0;
-  b3w->x_dll = b3w->x_drr = 1.0;
-  b3w->x_dlr = b3w->x_drl = 0.0;
+	b3w->x_drum_width = 0.0;
+	b3w->x_dll = b3w->x_drr = 1.0;
+	b3w->x_dlr = b3w->x_drl = 0.0;
 
-  return (LV2_Handle)b3w;
+	return (LV2_Handle)b3w;
 }
 
 static void
@@ -280,7 +281,8 @@ connect_port (LV2_Handle instance,
 
     case B3W_GUINOTIFY:   b3w->p_resend_trigger = (float*)data; break;
     case B3W_LINKSPEED:   b3w->p_link_speed = (float*)data; break;
-		default: break;
+    case B3W_MICANGLE:    b3w->mic_angle = (float*)data; break;
+    default: break;
   }
 }
 
@@ -377,10 +379,10 @@ static int interpolate_filter (B3W *b3w,  Filter *flt) {
 	return 0;
 }
 
-#define SETVAR(PARAM, VAR, MIN, MAX) { \
+#define SETVAR(PARAM, VAR, MIN, MAX, MOD) { \
 	const float val = *b3w->VAR; \
 	 b3w->o_ ## VAR = val; \
-	if (val >= MIN && val <= MAX) { b3w->whirl->PARAM = val; } \
+	if (val >= MIN && val <= MAX) { b3w->whirl->PARAM = MOD val; } \
 }
 
 #define CHECKDIFF(VAR) \
@@ -394,6 +396,7 @@ static int reconfigure (B3W* b3w) {
 	CHECKDIFF(horn_xoff);
 	CHECKDIFF(horn_zoff);
 	CHECKDIFF(mic_dist);
+	CHECKDIFF(mic_angle);
 
 	if (!changed) {
 		return 0;
@@ -403,11 +406,12 @@ static int reconfigure (B3W* b3w) {
 		return 1;
 	}
 
-	SETVAR(hornRadiusCm, horn_radius, 9, 50)
-	SETVAR(drumRadiusCm, drum_radius, 9, 50)
-	SETVAR(micDistCm, mic_dist, 9, 300)
-	SETVAR(hornXOffsetCm, horn_xoff, -20 , 20)
-	SETVAR(hornZOffsetCm, horn_zoff, -20 , 20)
+	SETVAR(hornRadiusCm, horn_radius, 9, 50,)
+	SETVAR(drumRadiusCm, drum_radius, 9, 50,)
+	SETVAR(micDistCm, mic_dist, 9, 300,)
+	SETVAR(hornXOffsetCm, horn_xoff, -20 , 20,)
+	SETVAR(hornZOffsetCm, horn_zoff, -20 , 20,)
+	SETVAR(micAngle, mic_angle, 0 , 180, 1.f - 1.f/180.f *)
 
 	computeOffsets (b3w->whirl);
 	return 0;

b_whirl/whirl.h
@@ -93,6 +93,8 @@ struct b_whirl {
   double hornAngleGRD;  /* 0..1 */
   double drumAngleGRD;
 
+  double micAngle;
+
 /* target speed - rotational frequency */
   float hornRPMslow;
   float hornRPMfast;

commit ded918d3a23387f41763de7ecaba8b22379f8dc7

fix rounding error introduced in 87d0ceb
which resulted in too bright leslie horn sound

b_whirl/whirl.c
@@ -1232,7 +1232,7 @@ void whirlProc2 (struct b_whirl *w,
     const unsigned int hl = (unsigned int)floor(h1) & WHIRL_DISPLC_MASK;   \
     const unsigned int hh = (hl + 1) & WHIRL_DISPLC_MASK;                  \
     const float intp = DSP[hl] * (1.f - hd) + hd * DSP[hh];                \
-    const unsigned int k = (unsigned int) floor(hd);                       \
+    const unsigned int k = ((unsigned int) round(h1)) & WHIRL_DISPLC_MASK; \
     const float t = hornSpacing[(P)] + intp + (float) outpos;              \
     const float r = floorf (t);                                            \
     float xa;                                                              \